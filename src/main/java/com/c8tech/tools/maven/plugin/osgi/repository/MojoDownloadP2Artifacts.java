905e35701d3634f9c6d79a236a058678e2331963
Cristiano V. Gavião <cvgaviao@gmail.com>
2019-06-07 00:28:44 -0300

    upgrade to new parent pom version
    changed license headers

--- src/main/java/com/c8tech/tools/maven/plugin/osgi/repository/MojoDownloadP2Artifacts.java
+++ src/main/java/com/c8tech/tools/maven/plugin/osgi/repository/MojoDownloadP2Artifacts.java
@@ -1,17 +1,16 @@
-/**
- * ==========================================================================
- * Copyright © 2015-2018 Cristiano Gavião, C8 Technology ME.
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- * Cristiano Gavião (cvgaviao@c8tech.com.br)- initial API and implementation
- * ==========================================================================
- */
 package com.c8tech.tools.maven.plugin.osgi.repository;
+/**
+ * ============================================================================
+ *  Copyright ©  2015-2019,    Cristiano V. Gavião
+ *
+ *  All rights reserved.
+ *  This program and the accompanying materials are made available under
+ *  the terms of the Eclipse Public License v1.0 which accompanies this
+ *  distribution and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * ============================================================================
+ */
+
 
 import java.io.File;
 import java.io.IOException;
